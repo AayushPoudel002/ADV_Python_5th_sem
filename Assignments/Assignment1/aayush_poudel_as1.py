82a4c637c22013aa2c006182502164646d0a9c67
AayushPoudel002 <poudelaayush47@gmail.com>
2025-12-19 16:05:17 +0545

    Assignment1_submitted

---
+++ Assignments/Assignment1/aayush_poudel_as1.py
@@ -0,0 +1,42 @@
+#Variables and inputs
+my_age = 21
+print("My age is:",my_age)
+
+fav_food=input("Enter you Favourite Food:")
+print("His Favourite Food is:",fav_food)
+
+
+# Type Conversion
+
+String="42"
+integer=int(String)
+
+print("string convereted to integer:",integer)
+
+float_value=3.14159
+String2=str(float_value)
+
+print("Float Converted to string:",String2)
+
+
+#Strings
+
+str1="Hello"
+str2="World!"
+print(str1+str2)
+
+
+#Use String indexing to extract the third character from string "Python"
+str3="Python"
+third_char = str3[2]
+print("Third character in",str3,"is",third_char)
+
+#take a sentence as input adn display only the first five words
+
+sentence = input("ENter a long sentence more than 5 words:")
+"I am Aayush and I am 20 yeard old"
+"I" "am" "Aayush" "and" "I" "am" "20" "yrs" "old"
+print(sentence)
+words = sentence.split()
+five_words = " ".join(words[:5])
+print(five_words)
